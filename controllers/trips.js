029007f23e3285b558ff2fef1a13d378196e9c3b
cfalcant <cfalcant@gmail.com>
2018-01-24 16:07:40 -0700

    completed story #5

--- controllers/trips.js
+++ controllers/trips.js
@@ -1,27 +1,37 @@
 const knex = require("../db/knex.js");
 
-// module.exports = {
-//   index: function(req, res) {
-//     res.render('trips');
-//   },
-//
-// }
-
 module.exports = {
   index: function(req, res) {
     knex('trips')
-      .where('user_id', req.session.user.id)
+    .join('flight', 'trips.flight_id', 'flight.id')
+      .where('trips.user_id', req.session.user.id)
       .then((results)=>{
-        knex('flight')
+        knex('users')
+        .where('id', req.session.user.id)
+        .then((user)=>{
+          knex('flight')
           .then((flightData)=>{
 
             res.render('trips', {trips:results, user:req.session.user, flight:flightData});
           })
       })
-
+})
   },
-  // newBooking: function (req, res) {
-  //   knex('trips')
+  newBooking: function (req, res) {
+    knex('trips')
+    .insert({
+      title:req.body.title,
+      description:req.body.description,
+      flight_id:req.body.flight_id,
+      user_id: req.session.user.id
+    })
+    // .where(req.body.user.id, req.session.user.id)
+    .then((newTrip)=>{
+      res.redirect('/trips')
+    })
+
 
 
   }
+
+}
